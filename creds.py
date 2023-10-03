15fa921de7affc4c1807eefd0815125723b6fdf5
vespinoza0 <vespinoza0391@gmail.com>
2023-10-02 23:14:09 -0400

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
-access_key_id= 'AKIAZZ33YB65GZIN656A'
-secret_access_key= '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'
-bucket_name = 'mindex-data-analytics-code-challenge'
+access_key_id= ''
+secret_access_key= ''
+bucket_name = ''
 database = {
-        'host':'ls-2619b6b15c9bdc80a23f6afb7eee54cf0247da21.ca3yee6xneaj.us-east-1.rds.amazonaws.com',
-        'username' : 'victor_espinoza',
-        'password' : 'eictorvspinoza',
-        'database' : 'postgres'
+        'host':'',
+        'username' : '',
+        'password' : '',
+        'database' : ''
         }
